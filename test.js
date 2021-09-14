da4ac32f8ea5fd80dc7d1bef2443daa83367b760
Andy <smsbtbxdcl@gmail.com>
2021-09-14 13:13:32 +0800

    test分支代码

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-console.log('这是测试代码')
-console.log('这是dev分支代码')
-
-
-var  m = [4,2,2,7];
-var n = [false,false,false,false,];
-
-for(let i=0; i<m.length; i++) {
-    for(let j=i+1; j<m.length; j++) {
-        if( m[i] === m[j] ){
-            n[j] = true;
-            n[i] = true
-        }
-    }
-}
-console.log(n)
 //test分支代码
